b87c4140a866338083718b290443c5ac00e98317
michaelboegner <cmikeb@gmail.com>
2024-09-15 16:35:44 -0400

    Users.vue successfully calls backend interviewer service and returns list of users

--- interviewer/src/App.vue
+++ interviewer/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Users from './components/Users.vue'
 </script>
 
 <template>
@@ -8,12 +7,12 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it indeed!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <Users />
   </main>
 </template>
 
---
+++ interviewer/src/components/Users.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>User List</h2>
+    <ul>
+      <li v-for="user in users" :key="user.Id">
+        {{ user.Username }} 
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const users = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/api/users');
+    console.log("RESPONSE:", response, "\n")
+    users.value = response.data.users;
+  } catch (error) {
+    console.error('Failed to fetch users', error);
+  }
+});
+</script>
+
+<style>
+/* Add your styles here */
+</style>
